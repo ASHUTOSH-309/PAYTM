7288818e9fd89da2f1fea93de4d7e205c04f7da1
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-31 17:51:46 +0530

    Basic Structure laid

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,23 @@
 const express = require("express");
+const cors=require("cors");
+const app=express()
+
+
+app.use(cors())
+app.use(express.json())
+
+const mainRouter=require("./routes/index")
+
+
+
+
+
+app.use("/api/v1",mainRouter)
+app.listen(3000);
+// /api/v1/user/signup
+// /api/v1/user/signin
+// /api/vq/user/changePassword
+// /api/v1/account/balance
+
 
 
