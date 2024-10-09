b31085d006b51b33726d3f91b3829513f4a9953d
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-10-09 18:49:05 +0530

    SignIn, Signup, update, getUsers route created

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,13 +2,14 @@ const express = require("express");
 const { User } = require("../db")
 const zod = require("zod");
 const router = express.Router();
-const jwt=require("jsonwebtoken");
-const JWT_SECRET=require("../config")
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
+const { authMiddleware } = require("../middleware");
 
 const signupSchema = zod.object({
     username: zod.string().email(),
-    password: zod.string(),
     firstName: zod.string(),
+    lastName: zod.string(),
     password: zod.string()
 })
 
@@ -37,22 +38,29 @@ router.post("/signup", async (req, res) => {
     //Now when inputs are valid and there exists no user with the same email, so we can
     //inintiate a query to create a user in the database
 
-    const dbUser=await User.create(body)
-    // token=(Header.payload.signature)
-    const token=jwt.sign({
-        userId:dbUser._id
-    },JWT_SECRET)
+    const dbUser = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
 
-    res.json({
+
+    // token=(Header.payload.signature)
+    const token = jwt.sign({
+        userId: dbUser._id
+    }, JWT_SECRET)
+
+    return res.json({
         message: "User created Succesfully",
-        token:token
+        token: token
     })
 
 })
 
 const signinBody = zod.object({
     username: zod.string().email(),
-	password: zod.string()
+    password: zod.string()
 })
 
 router.post("/signin", async (req, res) => {
@@ -73,15 +81,86 @@ router.post("/signin", async (req, res) => {
             userId: user._id
         }, JWT_SECRET);
 
-        res.json({
+        return res.json({
+            message: "User Logged In Succesfully",
             token: token
         })
-        return;
+
     }
 
 
-    res.status(411).json({
+    return res.status(411).json({
         message: "Error while logging in"
+    })
+})
+
+
+const UpdateSchema = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put("/", authMiddleware, async (req, res) => {
+
+    const body = req.body;
+
+    const { success } = UpdateSchema.safeParse(body)
+    //Check for schema validation
+    if (!success) {
+        return res.status(411).json({
+            message: "Invalid Inputs/Error While updating"
+        })
+    }
+ 
+    await User.updateOne(req.body, {
+        _id: req.userId
+    })
+
+    res.json({
+        message: "Updated successfully"
+    })
+
+
+})
+
+/* 
+This is needed so users can search for their friends and send them money
+
+Method: GET
+Route: /api/v1/user/bulk
+Query Parameter: `?filter=harkirat`
+
+Response:
+
+Status code - 200
+*/
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+
+    /* 
+        crux is show the results for which filter exists as a substring either in firstname or lastname 
+    */
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
     })
 })
 
