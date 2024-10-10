6832fb98e8c46126e380ab5b0d77a041df9ccbc9
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-10-10 20:23:25 +0530

    Add routing to the react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Dashboard from "./components/Dashboard"
+import SendMoney from "./components/SendMoney"
+import Signin from "./components/Signin"
+import Signup from "./components/Signup"
 
-  return (
-    <div>
-        Project Bootstarped
-    </div>
-  )
-}
+
+
+  function App() {
+    return (
+      <>
+         <BrowserRouter>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/send" element={<SendMoney />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    )
+  }
+
 
 export default App
