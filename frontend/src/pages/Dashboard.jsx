51ebf5eb364bcb87a21ac450e0dbfdbe2f8dc69e
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-10-11 21:11:17 +0530

    Logic to show the balance of logged in user making a network request

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,43 @@
+import { useEffect, useState } from "react";
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import axios from "axios";
 
 export const Dashboard = () => {
-    return <div>
-        <Appbar />
-        <div className="m-8">
-            <Balance value={"10,000"} />
-            <Users />
-        </div>
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+
+
+
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:3000/api/v1/account/balance',
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzA3YjJmMjY3NDQyNmIxMDFiM2FhMTEiLCJpYXQiOjE3Mjg2NjA3NzN9.b6J4PPidIzrlHFsVOm3i7xMc-gAeVoKhAKSbt-pFeLU'
+      }
+    };
+
+    axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        setBalance(response.data.balance)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+
+  }, [balance])
+
+
+  return <div>
+    <Appbar />
+    <div className="m-8">
+      <Balance value={balance.toFixed(2)} />
+      <Users />
     </div>
+  </div>
 }
