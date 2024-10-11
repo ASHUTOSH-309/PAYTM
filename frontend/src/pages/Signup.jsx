9ee3fc79848ddc2c35974015cac33f52b91f12a8
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-10-11 16:07:08 +0530

    Server succesfully returning the authorization token

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Heading } from '../components/Heading';
 import { SubHeading } from '../components/SubHeading';
 import { InputBox } from '../components/InputBox';
@@ -15,8 +15,8 @@ const Signup = () => {
         e.preventDefault(); // Prevents the default form submission
 
         const payload = {
-            firstName:firstname,
-            lastName:lastname,
+            firstName: firstname,
+            lastName: lastname,
             username: Username,
             password: Password,
         };
@@ -42,22 +42,52 @@ const Signup = () => {
         }
     };
 
+
+
     return <div className="bg-slate-300 h-screen flex justify-center">
-    <div className="flex flex-col justify-center">
-      <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-        <Heading label={"Sign up"} />
-        <SubHeading label={"Enter your infromation to create an account"} />
-        <InputBox placeholder="John" label={"First Name"} />
-        <InputBox placeholder="Doe" label={"Last Name"} />
-        <InputBox placeholder="ashutosh@gmail.com" label={"Email"} />
-        <InputBox placeholder="123456" label={"Password"} />
-        <div className="pt-4">
-          <Button label={"Sign up"} />
+        <div className="flex flex-col justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                <Heading label={"Sign up"} />
+                <SubHeading label={"Enter your infromation to create an account"} />
+
+
+                <InputBox placeholder="John" label={"First Name"} onChange={(e) => {
+                    setfirstName(e.target.value)
+                }} />
+
+
+
+                <InputBox placeholder="Doe" label={"Last Name"}
+                    onChange={(e) => {
+                        setlastName(e.target.value)
+                    }}
+                />
+
+
+                <InputBox placeholder="ashutosh@gmail.com" label={"Email"}
+                    onChange={(e) => {
+                        setUserName(e.target.value)
+
+                    }}
+                />
+
+
+                <InputBox placeholder="123456" label={"Password"}
+                    onChange={
+                        (e) => {
+                            setPassword(e.target.value)
+                        }
+                    }
+                />
+
+
+                <div className="pt-4">
+                    <Button label={"Sign up"} onClick={handleSignup} />
+                </div>
+                <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
+            </div>
         </div>
-        <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
-      </div>
     </div>
-  </div>
 }
 
 export default Signup;
